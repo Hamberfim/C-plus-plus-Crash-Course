ab47594e97670e40396d1e330ff048f53b8003f0
hamberfim <adhamlin@gmail.com>
2024-02-10 12:54:01 -0600

    class methods access

--- 02_types/classMethods.cpp
+++ 02_types/classMethods.cpp
@@ -2,6 +2,13 @@
 
 // class with a method as a member
 struct TrackYear {
+  // constructor
+  TrackYear(int year_in) {
+    if (!set_year(year_in)) {
+      year = 2023;
+    }
+  }
+
   void add_year() { year++; }
 
   bool set_year(int new_year) {
@@ -18,10 +25,13 @@ private:
 };
 
 int main(void) {
-  TrackYear someYear;
-  if (!someYear.set_year(2018)) {
-    someYear.set_year(2023);
-  }
+  //   TrackYear someYear;
+  //   if (!someYear.set_year(2018)) {
+  //     someYear.set_year(2023);
+  //   }
+
+  // using the constructor
+  TrackYear someYear{1966};
 
   someYear.add_year();
   printf("Year: %d\n", someYear.get_year());
