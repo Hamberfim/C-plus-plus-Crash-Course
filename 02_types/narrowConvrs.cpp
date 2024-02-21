72c60f1abc20d3ef56308c09c8909222e7fe2494
hamberfim <adhamlin@gmail.com>
2024-02-21 15:25:16 -0600

    narrowing conversions

---
+++ 02_types/narrowConvrs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  // brace initializations generate warnings when
+  // implicit narrowing conversions are encountered
+  float a{1};
+  float b{2};
+
+  int narrowed_result(a / b); // bad narrowing conversion
+  cout << narrowed_result << endl;
+
+  /* error: narrowing conversion of ‘(a / b)’ from ‘float’ to ‘int’
+   [-Werror=narrowing] 12 |   int result{a / b}; // warning generated */
+  int result{a / b}; // result 0.5 but truncated to 0
+  cout << result << endl;
+
+  return 0;
+}
